e918b576031942631614298598ffbb92b6fc9ebc
Abdul Jaweed <jdgaming7320@gmail.com>
2023-03-16 21:49:05 +0530

    updating model_trainer.py file and performing hyperparameter

--- student/components/data_ingestion.py
+++ student/components/data_ingestion.py
@@ -64,8 +64,8 @@ if __name__ == "__main__":
     obj=DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
     
-    data_transformation = DataTransformation()
-    train_arr, test_arr_ = data_transformation.initiate_data_transformation(train_data,test_data)
-    
-    modeltrainer = ModelTrainer()
-    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
--- student/utils.py
+++ student/utils.py
@@ -53,3 +53,6 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise StudentException(e, sys)
