6490ebaa574455f8ba4d7916bfd3d414d427a803
Abdul Jaweed <jdgaming7320@gmail.com>
2023-03-16 22:50:10 +0530

    app.py file added and predict_pipeline.py is updated

--- student/utils.py
+++ student/utils.py
@@ -56,3 +56,13 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
     except Exception as e:
         raise StudentException(e, sys)
+
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise StudentException(e, sys)
